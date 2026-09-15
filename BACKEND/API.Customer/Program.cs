acf6d1a0ae3783288038de74287929c4823f8a87
ThanhTran(KaitoKid) <thanhtran2005isme@gmail.com>
2026-09-15 08:01:45 +0700

    Bổ sung CORS cho Expo Web khi phát triển mobile

--- BACKEND/API.Customer/Program.cs
+++ BACKEND/API.Customer/Program.cs
@@ -42,7 +42,9 @@ builder.Services.AddCors(options =>
                 "http://localhost:5173",
                 "http://localhost:5174",
                 "http://localhost:3000",
-                "http://127.0.0.1:5173"
+                "http://localhost:8081",
+                "http://127.0.0.1:5173",
+                "http://127.0.0.1:8081"
             )
             .AllowAnyHeader()
             .AllowAnyMethod()
